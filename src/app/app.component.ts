23f1c698315708d0ff0adeeb1f0daba94e3c5093
Avanade <Avanade@WIN-Q7AHOIVCJY.corp.avanade.org>
2019-07-02 02:43:35 -0300

    upload arquivo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'gama-angular-exercicios';
+  count: number = 0;
+  numberRandom: number  = 0;
+
+  addCount() {
+    if (this.count !== 100) {
+      this.count++;
+    }
+  }
+
+  subCount() {
+    if (this.count !== 0) {
+      this.count--;
+    }
+  }
+
+  randomNumber() {
+    this.numberRandom = Math.floor(1 + Math.random()*(100 + 1 - 1))
+  }
+
+
+
 }
